70a91e4750843907af6f78cad3424a135dafa071
Your Name <you@example.com>
2020-10-15 15:42:00 -0500

    first commit

---
+++ src/test/java/BDD/StepsDef/StudentDefs.java
@@ -0,0 +1,147 @@
+package BDD.StepsDef;
+
+import Base.BasePage;
+import Pages.StudentsPage;
+import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import org.testng.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDefs extends BasePage {
+
+    public StudentsPage studentsPage;
+
+    @Before
+    public void setUp() throws InterruptedException {
+        initialization();
+        studentsPage=new StudentsPage();
+    }
+
+    @Given("^User is in student menu$")
+    public void userIsInStudentMenu() throws InterruptedException {
+        studentsPage.navigateStudentsmenu();
+        Thread.sleep(5000);
+    }
+
+    @When("^User clicks on new student link$")
+    public void userClicksOnNewStudentLink() throws InterruptedException {
+        studentsPage.clickCreateNew();
+        Thread.sleep(5000);
+    }
+
+    @Then("^User verifies create heading$")
+    public void userVerifiesCreateHeading() {
+        studentsPage.verifyHeadingCreate();
+    }
+
+    @When("^User enters last name$")
+    public void userEntersLastName() throws InterruptedException {
+        studentsPage.enterLastName();
+        Thread.sleep(3000);
+    }
+
+    @And("^User enters first name$")
+    public void userEntersFirstName() throws InterruptedException {
+        // Enter forms Value
+        studentsPage.enterFirstName();
+        Thread.sleep(3000);
+    }
+
+
+    @And("^User enters enrollment date$")
+    public void userEntersEnrollmentDate() throws InterruptedException {
+        studentsPage.enterEnrollmentDate();
+        Thread.sleep(3000);
+    }
+
+    @And("^User click on create button$")
+    public void userClickOnCreateButton() {
+        studentsPage.clickCreateButton();
+    }
+
+    @Then("^Verify User added is listed on the student list$")
+    public void verifyUserAddedIsListedOnTheStudentList() throws InterruptedException {
+        //
+        String searchStudent="Nirman";
+        byte count=0;
+        List<String> students;
+        students=studentsPage.getAllStudentsNameList();
+        for(String eachStudent:students){
+            System.out.println(eachStudent);
+            if(eachStudent.equals(searchStudent)) {
+                count++;
+            }
+            if(count>0)
+                break;
+            else
+                continue;
+        }
+        //if count >0, we found the user
+        if(count!=0)
+            Assert.assertTrue(true);
+        else
+            Assert.assertFalse(false);
+    }
+
+    @When("^User enters find by name as \"([^\"]*)\"$")
+    public void userEntersFindByNameAs(String name) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        // new PendingException();
+        studentsPage.enterNameSearchButton(name);
+
+    }
+
+    @And("^User clicks on search button$")
+    public void userClicksOnSearchButton() {
+        studentsPage.clickSearchButton();
+
+    }
+
+    @And("^User clicks on delete button$")
+    public void userClicksOnDeleteButton() {
+        studentsPage.clickAllDeleteButtons();
+    }
+
+    @And("^User clicks on confirm delete button$")
+    public void userClicksOnConfirmDeleteButton() {
+        studentsPage.clickDeleteConfirm();
+    }
+
+    @When("^User enters find by name as \"([^\"]*)\" and delete all from list$")
+    public void userEntersFindByNameAsAndDeleteAllFromList(String name) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        boolean flag=true;
+        while(flag==true) {
+            studentsPage.enterNameSearchButton(name);
+            studentsPage.clickSearchButton();
+            if(studentsPage.getDeteleteButtonsSize()>0) {
+                studentsPage.clickDeleteButtons();
+                studentsPage.clickDeleteConfirm();
+            }
+            else
+            {
+                break;
+            }
+        }
+
+    }
+
+    @Then("^Verify user \"([^\"]*)\" is not in the student list$")
+    public void verifyUserIsNotInTheStudentList(String arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @After
+    public void exit(){
+        wd.quit();
+    }
+
+}
---
+++ src/test/java/Pages/StudentsPage.java
@@ -0,0 +1,156 @@
+package Pages;
+
+import Base.BasePage;
+import Common.CommonPage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class StudentsPage extends BasePage {
+
+    CommonPage cp=new CommonPage();
+
+    @FindBy(xpath="//a[text()='Students']")
+    WebElement studentsMenu;
+
+    @FindBy(xpath = "/html/body/div/p/a")
+    WebElement lnkCreateNew;
+
+    //Create Heading
+    @FindBy(xpath = "//h2[text()='Create']")
+    WebElement headingCreate;
+
+    //Students Info
+    @FindBy(id="LastName")
+    WebElement lastName;
+
+    @FindBy(id="FirstMidName")
+    WebElement firstName;
+
+    @FindBy(id="EnrollmentDate")
+    WebElement enrollmentDate;
+
+    @FindBy(xpath ="//input[@class='btn btn-default']")
+    WebElement createButton;
+
+    @FindBy(xpath = "/html/body/div[2]/table/tbody/tr/td[2]")
+    List<WebElement> eStudents;
+
+    @FindBy(xpath ="//a[text()='»']")
+    List<WebElement> nextButton;
+
+    @FindBy(id="SearchString")
+    WebElement eSearchTextBox;
+
+    @FindBy(xpath = "//input[@type='submit']")
+    WebElement eSearchButton;
+
+    @FindBy(xpath="/html/body/div[2]/table/tbody/tr/td[4]/a[3]")
+    List<WebElement> eDeleteButtons;
+
+    @FindBy(xpath="//input[@type='submit']")
+    WebElement eDeleteConfirm;
+
+
+    public StudentsPage() {
+        PageFactory.initElements(wd,this);
+    }
+
+    public void navigateStudentsmenu(){
+        studentsMenu.click();
+    }
+
+    public void clickCreateNew(){
+        lnkCreateNew.click();
+    }
+
+    public void clickSearchButton(){
+        eSearchButton.click();
+    }
+
+
+    public void verifyHeadingCreate(){
+        cp.isElementDisplayed(headingCreate);
+    }
+
+    public void enterFirstName(){
+        cp.enterData(firstName,prop.getProperty("FIRST_NAME"));
+    }
+
+    public void enterLastName(){
+        cp.enterData(lastName,prop.getProperty("LAST_NAME"));
+    }
+
+    public void enterEnrollmentDate(){
+        String pattern = "MM/dd/yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        String date = simpleDateFormat.format(new Date());
+        cp.enterData(enrollmentDate,date);
+    }
+
+    public void clickCreateButton(){
+        createButton.click();
+    }
+
+    public String getHomePageTitle(){
+        return wd.getTitle();
+    }
+
+
+    public List<String> getAllStudentsNameList() throws InterruptedException {
+        //System.out.println(students.size());
+        //click till the next button is disabled , add all the pages records
+        //Note: if nextButton is not kept in array, it then we can't use the size(), and
+        //since for single element, only
+        List<String> students=new ArrayList<>();
+        for(WebElement eStudent:eStudents) {
+            students.add(eStudent.getText());
+        }
+        while(nextButton.size() > 0 && nextButton.get(0).isDisplayed()==true){
+            nextButton.get(0).click();
+            for(WebElement eStudent:eStudents) {
+                students.add(eStudent.getText());
+            }
+           // Thread.sleep(3000);
+        }
+                return students;
+    }
+
+    public void enterNameSearchButton(String name) {
+        eSearchTextBox.sendKeys(name);
+    }
+
+    public void clickAllDeleteButtons() {
+        while (eDeleteButtons.size() > 0) {
+            clickDeleteButtons();
+            clickDeleteConfirm();
+        }
+    }
+
+    public void clickDeleteButtons(){
+        eDeleteButtons.get(0).click();
+    }
+
+    public int getDeteleteButtonsSize(){
+        return eDeleteButtons.size();
+    }
+
+    public void clickDeleteConfirm(){
+        eDeleteConfirm.click();
+    }
+
+    public void close(){
+        wd.quit();
+    }
+
+}
+
+
+
